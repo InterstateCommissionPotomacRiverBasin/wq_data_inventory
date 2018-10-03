5615ddde2937a977b28038ede0910db879bd4463
Erfaneh Sharifi <ESharifi@icprb.int>
2018-10-03 17:32:36 -0400

    not necassary columns deleted

--- shiny/shiny_wqdi/global.R
+++ shiny/shiny_wqdi/global.R
@@ -38,8 +38,8 @@ inventory.df <- suppressWarnings(
 meta.df <- suppressWarnings(
   data.table::fread("data/icprb_metadata.csv",
                                               showProgress = FALSE)) %>%               
-  standard_names() %>% 
-  dplyr::rename(organization = "originator")
+  standard_names()#%>% 
+#dplyr::rename(organization = "originator")
 
 inventory.df <- full_join(inventory.df, meta.df, by = c("source_no"))
 inventory.df[inventory.df == "N/A"] <- "Unavailable"
@@ -48,18 +48,17 @@ rm(meta.df)
 
 
 
-program.cols <- c("organization", "program_name", "site_location",
+program.cols <- c("program_name", "site_location",
                   "purpose", "metric_parameter", "parameter_group",
                   "spatial_coverage", "fall_line", "lat_long",
                   "number_of_sites_sampled", "frequency_sampled",
                   "period_of_record_start_date", "period_of_record_end_date",
                   "collection_method", "update_frequency",
                   "public_or_restricted_data", "dataset_fees",
-                  "data_type", "program_website", "data_link",
-                  "contact_name", "contact_phone", "contact_email")
+                  "data_type", "program_website", "data_link")
 program.df <- inventory.df[, names(inventory.df) %in% program.cols]
 
-site.cols <- c("organization", "program name", "station_id", "lat", "long")
+site.cols <- c("program name", "station_id", "lat", "long")
 site.df <- inventory.df[, names(inventory.df) %in% site.cols]
 
 #map.df <- suppressWarnings(
